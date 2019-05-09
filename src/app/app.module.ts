149887e0202615c27046f5ab22161e94f01eae46
neelam.gupta@dotsquares.com <neelam.gupta@dotsquares.com>
2019-05-09 17:27:43 +0530

    created login module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/model/login.model.ts
@@ -0,0 +1,6 @@
+export class loginmodel {
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+}
