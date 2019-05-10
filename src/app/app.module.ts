2b4023dd923af554efaa6123e217341c6eec682c
jain.rajesh@dotsquares.com <[jain.rajesh@dotsquares.com}>
2019-05-10 15:09:36 +0530

    dashboard design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,49 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
-import { MatInputModule, MatButtonModule, } from '@angular/material';
+import {
+  MatInputModule,
+  MatButtonModule,
+  MatCardModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatNavList,
+  MatGridList,
+  MatGridTile,
+  MatDivider,
+  MatList,
+  MatTableModule,
+  MatDialogModule,
+  MatToolbarModule,
+  MatStepperModule,
+  MatTabsModule,
+  MatExpansionModule,
+  MatCheckboxModule,
+  MatTooltipModule,
+} from '@angular/material';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent],
+    LoginComponent,
+    DashboardComponent,
+    SidenavComponent,
+    MatNavList,
+    MatGridList,
+    MatGridTile,
+    MatDivider,
+    MatList,
+  ],
   imports: [
+    MatIconModule,
     BrowserModule,
     AppRoutingModule,
     FormsModule,
@@ -22,6 +53,22 @@ import { MatInputModule, MatButtonModule, } from '@angular/material';
     BrowserAnimationsModule,
      FlexLayoutModule,
      MatButtonModule,
+     MatCardModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatStepperModule,
+    MatTabsModule,
+    HttpClientModule,
+    MatExpansionModule,
+    MatCheckboxModule,
+    MatTableModule,
+    MatDialogModule,
+    MatTooltipModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
