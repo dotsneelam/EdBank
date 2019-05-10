ee28666e4ca11e8d0becaa634bf9dc9f3a42809e
neelam.gupta@dotsquares.com <neelam.gupta@dotsquares.com>
2019-05-10 10:32:37 +0530

    change sass to scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { JobsComponent } from './jobs/jobs.component';
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    DashboardComponent,
+    JobsComponent,
     
   ],
   imports: [
