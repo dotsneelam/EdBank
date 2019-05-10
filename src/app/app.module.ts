10fce209411c860c4957fdb5d51467fb0f9fa43c
jain.rajesh@dotsquares.com <[jain.rajesh@dotsquares.com}>
2019-05-10 11:08:50 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
+
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
+import { MatInputModule,MatButtonModule, } from '@angular/material';
+
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+     FlexLayoutModule,
+     MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
